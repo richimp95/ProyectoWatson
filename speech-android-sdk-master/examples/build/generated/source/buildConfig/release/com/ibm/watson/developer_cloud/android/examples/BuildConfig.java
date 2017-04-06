d3b6dbff0074138c20ae0c665f5ceec047e99c05
Ricardo Miranda Prado <Ricardo Miranda Prado>
2017-04-06 11:20:01 -0600

    Speech to text watson

---
+++ speech-android-sdk-master/examples/build/generated/source/buildConfig/release/com/ibm/watson/developer_cloud/android/examples/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ibm.watson.developer_cloud.android.examples;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.ibm.watson.developer_cloud.android.examples";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
